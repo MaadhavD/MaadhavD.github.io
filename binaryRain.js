1f9fc10d96c90ce0674b6ed00f50c35ad39f4080
MaadhavRobotics <maadhavdeekshitha@gmail.com>
2023-03-23 17:43:30 -0400

    Update binaryRain.js

--- binaryRain.js
+++ binaryRain.js
@@ -1,65 +1,62 @@
-const canvas = document.getElementById('binary-rain');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("binaryRainCanvas");
+const context = canvas.getContext("2d");
 
-// Set the canvas dimensions to match the window dimensions
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+let canvasWidth, canvasHeight, fontSize;
+let drops = [];
 
-// Define the characters to use for the raindrops
-const characters = '01';
+function setup() {
+  canvasWidth = window.innerWidth;
+  canvasHeight = window.innerHeight;
+  fontSize = canvasWidth / 20;
+  canvas.width = canvasWidth;
+  canvas.height = canvasHeight;
+  context.font = `${fontSize}px Arial`;
 
-// Define the font size and style for the raindrops
-const fontSize = 12;
-const fontStyle = 'monospace';
-
-// Define the number of columns and rows for the raindrops
-const numColumns = Math.floor(canvas.width / fontSize);
-const numRows = Math.floor(canvas.height / fontSize);
-
-// Create an array to store the y-position of each raindrop
-const drops = [];
-
-// Initialize the y-position of each raindrop to a random value
-for (let i = 0; i < numColumns; i++) {
-  drops[i] = Math.floor(Math.random() * numRows);
-}
-
-// Define the function to draw the raindrops
-function drawRain() {
-  // Set the fill color to black
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
-
-  // Fill the canvas with black to create the fade effect
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  // Set the font size and style for the raindrops
-  ctx.font = `${fontSize}px ${fontStyle}`;
-
-  // Loop through each column of the canvas
-  for (let i = 0; i < numColumns; i++) {
-    // Generate a random character from the character set
-    const character = characters[Math.floor(Math.random() * characters.length)];
-
-    // Set the fill color to green
-    ctx.fillStyle = '#21d28e';
-
-    // Draw the raindrop at the current position
-    ctx.fillText(character, i * fontSize, drops[i] * fontSize);
-
-    // Move the raindrop down by one row
-    drops[i]++;
-
-    // If the raindrop has moved off the bottom of the canvas, reset its position to the top
-    if (drops[i] * fontSize > canvas.height && Math.random() > 0.975) {
-      drops[i] = 0;
-    }
+  for (let i = 0; i < canvasWidth / fontSize; i++) {
+    drops.push(new Drop(i * fontSize, -fontSize, fontSize));
   }
 }
 
-// Call the drawRain function repeatedly to create the animation
-setInterval(drawRain, 33);
+function draw() {
+  context.fillStyle = "rgba(0, 0, 0, 0.05)";
+  context.fillRect(0, 0, canvasWidth, canvasHeight);
 
-window.addEventListener('load', function() {
-  runBinaryRain();
-});
+  for (let i = 0; i < drops.length; i++) {
+    drops[i].update();
+    drops[i].show();
+  }
 
+  requestAnimationFrame(draw);
+}
+
+class Drop {
+  constructor(x, y, fontSize) {
+    this.x = x;
+    this.y = y;
+    this.fontSize = fontSize;
+    this.speed = Math.floor(Math.random() * 10) + 1;
+    this.switchInterval = Math.floor(Math.random() * 100);
+    this.letters = "01";
+    this.currentLetter = this.letters[Math.floor(Math.random() * this.letters.length)];
+  }
+
+  update() {
+    this.y += this.speed;
+
+    if (this.y > canvasHeight) {
+      this.y = -this.fontSize;
+      this.switchInterval = Math.floor(Math.random() * 100);
+      this.currentLetter = this.letters[Math.floor(Math.random() * this.letters.length)];
+    } else if (frameCount % this.switchInterval == 0) {
+      this.currentLetter = this.letters[Math.floor(Math.random() * this.letters.length)];
+    }
+  }
+
+  show() {
+    context.fillStyle = "#0F0";
+    context.fillText(this.currentLetter, this.x, this.y);
+  }
+}
+
+setup();
+draw();
