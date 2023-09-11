612d5831d011c665cdbab63d7e75e16b563d2957
Maadhav Deekshitha <maadhavdeekshitha@gmail.com>
2023-09-11 19:42:53 -0400

    Update binaryRain.js

--- binaryRain.js
+++ binaryRain.js
@@ -2,25 +2,20 @@ const canvas = document.getElementById("binaryRainCanvas");
 const context = canvas.getContext("2d");
 let frameCount = 0;
 
-// Setting the width and height of the canvas
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// Setting up the letters
 var letters = '01';
 letters = letters.split('');
 
-// Setting up the columns
 var fontSize = 15,
     columns = canvas.width / fontSize;
 
-// Setting up the drops
 var drops = [];
 for (var i = 0; i < columns; i++) {
   drops[i] = 1;
 }
 
-// Setting up the draw function
 function draw() {
   context.fillStyle = 'rgba(0, 0, 0, .1)';
   context.fillRect(0, 0, canvas.width, canvas.height);
